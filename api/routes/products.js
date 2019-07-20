65b79d758f72551ef36c60b6b62254469cc3d692
Sayid Haidar <sayyid.abdul.aziz.haidar@gmail.com>
2019-07-20 14:05:16 +0700

    create model and crud product

--- api/routes/products.js
+++ api/routes/products.js
@@ -6,9 +6,28 @@ const router = express.Router();
 const Product = require("../models/product");
 
 router.get("/", (req, res, next) => {
-  res.status(200).json({
-    msg: "Stauts OK!"
-  });
+  Product.find()
+    .exec()
+    .then(docs => res.status(200).json(docs))
+    .catch(err => res.status(500).json({ error: err }));
+});
+
+router.get("/:id", (req, res, next) => {
+  const id = req.params.id;
+  Product.findById(id)
+    .exec()
+    .then(doc => {
+      console.log(doc);
+      if (doc) {
+        res.status(200).json(doc);
+      } else {
+        res.status(404).json({ msg: "Not found!" });
+      }
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: err });
+    });
 });
 
 router.post("/", (req, res, next) => {
@@ -20,13 +39,38 @@ router.post("/", (req, res, next) => {
 
   product
     .save()
-    .then(result => console.log(result))
-    .catch(err => console.log(err));
+    .then(result => {
+      res.status(200).json({
+        msg: "method /POST to /Product Done!",
+        createProduct: result
+      });
+    })
+    .catch(err => {
+      res.status(500).json({
+        msg: "method /POST to /Product Err",
+        error: err
+      });
+    });
+});
 
-  res.status(200).json({
-    msg: "/POST OK!",
-    createProduct: product
-  });
+router.patch("/:id", (req, res, next) => {
+  const id = req.params.id;
+  const updateOps = {};
+  for (const ops of req.body) {
+    updateOps[ops.propName] = ops.value;
+  }
+  Product.update({ _id: id }, { $set: updateOps })
+    .exec()
+    .then(result => res.status(200).json(result))
+    .catch(err => res.status(500).json({ error: err }));
+});
+
+router.delete("/:id", (req, res, next) => {
+  const id = req.params.id;
+  Product.remove({ _id: id })
+    .exec()
+    .then(result => res.status(200).json(result))
+    .catch(err => res.status(500).json({ error: err }));
 });
 
 module.exports = router;
